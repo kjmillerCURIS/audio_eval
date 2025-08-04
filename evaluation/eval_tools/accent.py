1249868090c4b93748259d4af1ed22a3f2f8fa60
Arjun Chandra <ac25@bu.edu>
2025-08-04 19:34:01 -0400

    accent classifier

---
+++ evaluation/eval_tools/accent.py
@@ -0,0 +1,26 @@
+from evaluation.config import HF_CACHE_PATH
+
+import torchaudio
+from speechbrain.inference import EncoderClassifier
+
+def get_accent(audio_path):
+    """
+    - Speechbrain classifier from this repo - https://github.com/JuanPZuluaga/accent-recog-slt2022 
+    - Outputs cosine sim scores between class embeddings and audio embeddings
+    """
+    classifier = EncoderClassifier.from_hparams(
+        source="Jzuluaga/accent-id-commonaccent_ecapa", 
+        savedir=HF_CACHE_PATH + "/models--sb-accent" 
+    )
+
+    cos_sim_scores, score, index, text_lab = classifier.classify_file(audio_path)
+
+    accent_labels = [
+        'england', 'us', 'canada', 'australia', 'indian', 'scotland', 'ireland',
+        'african', 'malaysia', 'newzealand', 'southatlandtic', 'bermuda',
+        'philippines', 'hongkong', 'wales', 'singapore'
+    ]
+
+    sim_dict = {label: round(float(cos_sim_scores[0][i]), 3) for i, label in enumerate(accent_labels)}
+
+    return sim_dict
